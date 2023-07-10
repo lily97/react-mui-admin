240664145c929bfd60e6f2fad082521cfd642c6e
李春雷 <lcl970307@163.com>
2023-07-10 18:30:06 +0800

    commit all

--- src/components/Layout/Menu/index.jsx
+++ src/components/Layout/Menu/index.jsx
@@ -1,44 +1,81 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import routerList from '../../../'
-export default function BasicMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  console.log(setAnchorEl);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    console.log(event.currentTarget);
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { HashRouter as Router, Link, Route } from 'react-router-dom';
+// import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+// import Link from "@mui/material/Link";
+
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import SendIcon from "@mui/icons-material/Send";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import StarBorder from "@mui/icons-material/StarBorder";
+
+import home from "../../../../src/views/home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+export default function NestedList() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
   };
 
-  return (
-    <div>
-      <Button
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
+  root.render(
+    <>
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
       >
-        Dashboard
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-    </div>
+        <ListItemButton>
+          <ListItemIcon>
+            <SendIcon />
+          </ListItemIcon>
+          <ListItemText primary="Sent mail" />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItemButton>
+
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+
+        <Collapse
+          in={open}
+          timeout="auto"
+          unmountOnExit
+          component={<Link to="/">Home</Link>}
+        >
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary="Starred" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+      </List>
+      <Router>
+        <Route path="/" exact component={home}></Route>
+      </Router>
+    </>
   );
 }
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -3,6 +3,7 @@ import "./index.less";
 import React from "react";
 import { render } from "react-dom";
 import { Router, Route, Link } from "react-router-dom";
+import login  from '../login'
 function MyButton() {
   return (
     <div>
